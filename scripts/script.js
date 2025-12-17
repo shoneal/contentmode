666a087e388d16a636285847917cf2fb30fcf5cd
Roman Pigunov <jerome.malone@yandex.ru>
2025-12-17 06:52:10 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,232 @@
 const gallery = {
   covers: {
-    "Asha Banks": [17, "2025-12-01", "trebuchet ms"],
-    "Isabela Merced": [17, "2025-09-01", "gil-sans"],
-    "Aja Naomi King": [8, "2025-07-01", "Impact, Chicago"],
-    "Emma Appleton": [14, "2024-12-01", "gil-sans"],
-    "Lashana Lynch": [6, "2024-10-01", "Andale Mono"],
-    "Demi Singleton": [30, "2024-02-01", "gil-sans"],
+    "Asha Banks": {
+      sum: 17,
+      date: "2025-12-01",
+      fontFamily: "trebuchet ms",
+      fontSize: "60px",
+    },
+    "Isabela Merced": {
+      sum: 17,
+      date: "2025-09-01",
+      fontSize: "50px",
+    },
+    "Aja Naomi King": {
+      sum: 8,
+      date: "2025-07-01",
+      fontFamily: "Impact, Chicago",
+      fontSize: "50px",
+      textTransform: "uppercase",
+      letterSpacing: "-0.02em",
+    },
+    "Emma Appleton": {
+      sum: 14,
+      date: "2024-12-01",
+      fontSize: "60px",
+    },
+    "Lashana Lynch": {
+      sum: 6,
+      date: "2024-10-01",
+      fontFamily: "Times",
+      fontSize: "71px",
+      fontWeight: "normal",
+    },
+    "Demi Singleton": {
+      sum: 30,
+      date: "2024-02-01",
+      fontWeight: "normal",
+    },
   },
   "CONTENTs man": {
-    "Noah Schnapp": [9, "2025-12-01", "Helvetica"],
-    "Corey Mylchreest": [8, "2025-08-01", "gil-sans"],
-    "Jason Isaacs": [12, "2025-03-01", "Impact, Chicago"],
-    "Alessandro Nivola": [9, "2024-12-01", "avenir-reg", "italic"],
-    "Daniel Zovatto": [6, "2024-10-01", "didot"],
-    "Brandon Sklenar": [6, "2024-08-01", "raleway", "italic"],
-    "Justin H. Min": [16, "2024-05-01", "gil-sans"],
-    "Chance Perdomo": [14, "2023-12-01", "gil-sans"],
-    "Phil Dunster": [15, "2023-04-01", "gil-sans"],
+    "Corey Mylchreest": {
+      sum: 8,
+      date: "2025-08-01",
+      fontSize: "62px",
+    },
+    "Jason Isaacs": {
+      sum: 12,
+      date: "2025-03-01",
+      fontFamily: "Impact, Chicago",
+      fontSize: "42px",
+      letterSpacing: "-0.02em",
+    },
+    "Alessandro Nivola": {
+      sum: 9,
+      date: "2024-12-01",
+      fontFamily: "avenir-reg",
+      fontSize: "58px",
+      fontStyle: "italic",
+    },
+    "Daniel Zovatto": {
+      sum: 6,
+      date: "2024-10-01",
+      fontFamily: "didot",
+      fontSize: "60px",
+      fontWeight: "normal",
+    },
+    "Brandon Sklenar": {
+      sum: 6,
+      date: "2024-08-01",
+      fontFamily: "raleway-italic",
+      fontSize: "58px",
+      fontStyle: "italic",
+    },
+    "Justin H. Min": {
+      sum: 16,
+      date: "2024-05-01",
+      fontSize: "50px",
+      fontWeight: "normal",
+    },
+    "Chance Perdomo": {
+      sum: 14,
+      date: "2023-12-01",
+      fontWeight: "normal",
+    },
+    "Phil Dunster": {
+      sum: 15,
+      date: "2023-04-01",
+    },
   },
   features: {
-    "Zoe Cipres": [7, "2025-02-24", "Impact, Chicago"],
-    "Ayden Mayeri": [12, "2024-08-01", "asenine", "italic"],
-    "Julian Kostov": [7, "2025-04-01", "gil-sans-light", "italic"],
-    "Desi Lydic": [7, "2023-12-31", "gil-sans"],
-    "Richa Moorjani": [6, "2023-12-01", "gil-sans"],
-    "Tyler Alvarez": [14, "2022-11-18", "genghis-khan"],
-    "Christine Ko": [17, "2022-10-01", "montserrat"],
-    "Julia Butters": [17, "2022-08-01", "Symbol"],
-    "Camila Perez": [24, "2021-12-01", "Belleza"],
-    "Daniel Henney": [10, "2022-03-14", "Impact, Chicago"],
-    "Jake Lacy": [12, "2021-07-01", "EB Garamond"],
-    "Mason Gooding": [17, "2020-07-01", "genghis-khan"],
-    "May Calamawy": [8, "2020-06-01", "Petit Formal Script", "italic"],
-    Herizen: [10, "2020-04-01", "Abril Fatface"],
-    "Laysla De Oliveira": [15, "2018-08-01", "Abril Fatface"],
+    "Zoe Cipres": {
+      sum: 7,
+      date: "2025-02-24",
+      fontFamily: "Impact, Chicago",
+      fontSize: "55px",
+      textTransform: "uppercase",
+      letterSpacing: "-0.02em",
+    },
+    "Ayden Mayeri": {
+      sum: 12,
+      date: "2024-08-01",
+      fontFamily: "asenine",
+      fontSize: "70px",
+      fontStyle: "italic",
+    },
+    "Julian Kostov": {
+      sum: 7,
+      date: "2025-04-01",
+      fontFamily: "gil-sans-light",
+      fontSize: "80px",
+      textTransform: "uppercase",
+      letterSpacing: "-0.02em",
+      fontStyle: "italic",
+    },
+    "Desi Lydic": {
+      sum: 7,
+      date: "2023-12-31",
+    },
+    "Richa Moorjani": {
+      sum: 6,
+      date: "2023-12-01",
+      fontWeight: "normal",
+    },
+    "Tyler Alvarez": {
+      sum: 14,
+      date: "2022-11-18",
+      fontFamily: "genghis-khan",
+      fontWeight: "normal",
+    },
+    "Christine Ko": {
+      sum: 17,
+      date: "2022-10-01",
+      fontFamily: "montserrat",
+    },
+    "Julia Butters": {
+      sum: 17,
+      date: "2022-08-01",
+      fontFamily: "Symbol",
+    },
+    "Camila Perez": {
+      sum: 24,
+      date: "2021-12-01",
+      fontFamily: "Belleza",
+      fontWeight: "normal",
+      textTransform: "uppercase",
+    },
+    "Daniel Henney": {
+      sum: 10,
+      date: "2022-03-14",
+      fontFamily: "Impact, Chicago",
+      textTransform: "uppercase",
+      letterSpacing: "0.4px",
+    },
+    "Jake Lacy": {
+      sum: 12,
+      date: "2021-07-01",
+      fontFamily: "EB Garamond",
+      fontSize: "24pt",
+      fontWeight: "normal",
+    },
+    "Mason Gooding": {
+      sum: 17,
+      date: "2020-07-01",
+      fontFamily: "genghis-khan",
+      fontSize: "69px",
+      textTransform: "uppercase",
+      letterSpacing: "-1.8px",
+    },
+    "May Calamawy": {
+      sum: 8,
+      date: "2020-06-01",
+      fontFamily: "Petit Formal Script",
+      fontSize: "42px",
+      letterSpacing: "0.7px",
+      fontStyle: "italic",
+    },
+    Herizen: {
+      sum: 10,
+      date: "2020-04-01",
+      fontFamily: "Abril Fatface",
+      fontSize: "47px",
+      textTransform: "uppercase",
+      letterSpacing: "-1.8px",
+    },
+    "Laysla De Oliveira": {
+      sum: 15,
+      date: "2018-08-01",
+      fontFamily: "Abril Fatface",
+      fontSize: "48px",
+      textTransform: "uppercase",
+      letterSpacing: "-1.8px",
+    },
   },
   beauty: {
-    "Grace Van Patten": [15, "2021-10-01", "Archivo Black"],
-    "Hailey Kilgore": [14, "2021-09-01", "Arial Black"],
-    "Madelyn Cline": [18, "2021-08-01", "EB Garamond"],
+    "Maia Reficco": {
+      sum: 12,
+      date: "2025-02-01",
+      fontFamily: "Impact, Chicago",
+      fontSize: "55px",
+      textTransform: "uppercase",
+      letterSpacing: "-0.02em",
+    },
+    "Grace Van Patten": {
+      sum: 15,
+      date: "2021-10-01",
+      fontFamily: "Archivo Black",
+      fontSize: "26pt",
+      textTransform: "uppercase",
+    },
+    "Hailey Kilgore": {
+      sum: 14,
+      date: "2021-09-01",
+      fontFamily: "Arial Black",
+      fontSize: "18pt",
+      textTransform: "uppercase",
+    },
+    "Madelyn Cline": {
+      sum: 18,
+      date: "2021-08-01",
+      fontFamily: "Radiant",
+      fontSize: "76px",
+    },
   },
 };
 
 for (const section in gallery) {
   gallery[section] = Object.fromEntries(
     Object.entries(gallery[section]).sort(([, a], [, b]) =>
-      b[1].localeCompare(a[1])
+      b.date.localeCompare(a.date)
     )
   );
 }
@@ -263,8 +444,9 @@ document.addEventListener("click", (e) => {
   openPopup(popup);
 }); // Клик по обложкам
 const renderPopupContent = (name, section, data) => {
-  const img = popup.querySelector(".popup_header_image");
-  const title = popup.querySelector(".popup_header_title");
+  const popupHeader = popup.querySelector(".popup_header");
+  const img = popupHeader.querySelector(".popup_header_image");
+  const title = popupHeader.querySelector(".popup_header_title");
   const container = popup.querySelector(".popup_main");
 
   clearContainer(container);
@@ -272,16 +454,26 @@ const renderPopupContent = (name, section, data) => {
     name
   )}/768/main.jpg`;
   img.alt = name;
+  img.style.opacity = "0";
   img.addEventListener("load", () => {
+    if (img.naturalWidth > img.naturalHeight)
+      popupHeader.classList.add("popup_header_row");
+    else popupHeader.classList.remove("popup_header_row");
     img.style.opacity = "1";
   });
   title.textContent = name;
-  title.style.fontFamily = data[2];
-  title.style.fontStyle = data[3] || "normal";
+  title.style.fontFamily = data.fontFamily || "gil-sans";
+  title.style.fontSize = data.fontSize || "36pt";
+  title.style.fontWeight = data.fontWeight || "bolder";
+  title.style.textTransform = data.textTransform || "capitalize";
+  title.style.letterSpacing = data.letterSpacing || "normal";
+  title.style.fontStyle = data.fontStyle || "normal";
 
-  for (let i = 1; i <= data[0]; i++) {
-    const div = document.createElement("div");
-    div.className = "popup_image_wrapper";
+  const wrappers = [];
+
+  for (let i = 1; i <= data.sum; i++) {
+    const wrapper = document.createElement("div");
+    wrapper.className = "popup_image_wrapper";
 
     const imgEl = document.createElement("img");
     imgEl.src = `${basicLink}${getSectionPath(section)}/${formatName(
@@ -289,17 +481,82 @@ const renderPopupContent = (name, section, data) => {
     )}/768/${i}.jpg`;
     imgEl.alt = name;
     imgEl.loading = "lazy";
-    imgEl.onload = () => {
-      if (imgEl.naturalWidth > imgEl.naturalHeight) {
-        div.classList.add("horizontal_image");
-      } else {
-        div.classList.add("vertical_image");
-      }
-      imgEl.style.opacity = "1";
-    };
+    imgEl.style.opacity = "0";
 
-    div.append(imgEl);
-    container.append(div);
+    wrapper.append(imgEl);
+    container.append(wrapper);
+    wrappers.push({ wrapper, img: imgEl });
   }
+
+  const processVerticalGroup = (group) => {
+    group.forEach((wrapper, index) => {
+      if (index % 2 === 0) wrapper.classList.add("odd_vertical");
+      if (index % 2 === 0 && index + 1 >= group.length)
+        wrapper.classList.add("alone_image");
+    });
+  };
+
+  const processHorizontalSequence = (sequence) => {
+    const total = sequence.length;
+    if (total < 2) return;
+
+    const limit = total % 2 === 0 ? total : total - 1;
+
+    for (let i = 0; i < limit; i++) {
+      if (i % 2 === 0) sequence[i].classList.add("odd_horizontal");
+      else sequence[i].classList.add("even_horizontal");
+    }
+  };
+
+  const assignClasses = () => {
+    let verticalGroup = [];
+    let horizontalSequence = [];
+
+    wrappers.forEach(({ wrapper, img }, index) => {
+      const isHorizontal = img.naturalWidth > img.naturalHeight;
+
+      if (isHorizontal) {
+        wrapper.classList.add("horizontal_image");
+        horizontalSequence.push(wrapper);
+        if (verticalGroup.length > 0) {
+          processVerticalGroup(verticalGroup);
+          verticalGroup = [];
+        }
+      } else {
+        wrapper.classList.add("vertical_image");
+        verticalGroup.push(wrapper);
+        if (horizontalSequence.length > 0) {
+          processHorizontalSequence(horizontalSequence);
+          horizontalSequence = [];
+        }
+      }
+      if (index === wrappers.length - 1) {
+        processVerticalGroup(verticalGroup);
+        processHorizontalSequence(horizontalSequence);
+      }
+    });
+    container.style.opacity = "1";
+  };
+
+  Promise.all(
+    wrappers.map(({ img }) => {
+      return new Promise((resolve) => {
+        if (img.complete) {
+          img.style.opacity = "1";
+          resolve();
+        } else {
+          img.addEventListener("load", () => {
+            img.style.opacity = "1";
+            resolve();
+          });
+          img.addEventListener("error", () => {
+            img.style.opacity = "1";
+            resolve();
+          });
+        }
+      });
+    })
+  ).then(assignClasses);
 }; // Подгрузка инфы в popup
+
 closeBtn.addEventListener("click", () => closePopup(popup)); // Клик по закрытию popup
